0e648c62362f414cdd9ef0b300cf981ecedf4386
reginald1984 <reginalddennery1@gmail.com>
2026-05-17 22:49:30 -0400

    Update allowedOrigins.js

--- utils/allowedOrigins.js
+++ utils/allowedOrigins.js
@@ -3,6 +3,7 @@ const ALLOWED_ORIGINS = [
   'http://localhost:3000',
   'http://localhost:8280',
   'http://cheapcallme.com',
+    'https://cheapcallme.com',
   'https://campaign-lceh.onrender.com',  // Updated from cvtt to lceh
   'https://campaign-22qf.onrender.com'   // Keep old one for backward compatibility
 ];
